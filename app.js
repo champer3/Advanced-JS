8cf48fae516b21a7606ca9c12d5b77cac2292b16
champer3 <okunolas21@gmail.com>
2022-04-02 13:14:37 -0700

    sort and array copying in JavaScript

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ const names = ['stephen', 'bolu', 'ada', 'bisi']
 
 
 //MAP Returns a value and it is used to make modifications to an array
-const newNames = names.map(function(name){
-    return name.toUpperCase()
-})
-
-console.log(newNames)
-console.log(names)
+// const newNames = names.map(function(name){
+//     return name.length > 3 ? name : "nope"
+// })
 
 //FIND
 
@@ -15,4 +12,29 @@ const search = names.find(function(name){
     return name.includes('l')
 })
 
-console.log(search)
+
+//FILTER
+const shortSearch = names.filter(function(name){
+    return name.includes('b')
+})
+
+//Some and Every
+const checkLength = names.every(function(name){
+    return name.length>2
+})
+
+//ARROW FUNCTIONS
+const newNames = names.map(function(name){
+    return name.length > 3 ? name : "nope"
+})
+
+const arrowNames = names.map(name => name.length > 3 ? name : "nope")
+
+//SORT
+const items = ['banana', 'apple', 'orange', 'mango']
+const ratings = [92, 56, 33, 2, 48.7, 3, 9, 10]
+
+const desRatings = [...ratings]
+desRatings.sort((a,b) => b-a)
+console.log(desRatings)
+console.log(ratings)
