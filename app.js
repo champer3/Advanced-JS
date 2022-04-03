d634ad6698628c02710173caa9003bd6444ece11
champer3 <okunolas21@gmail.com>
2022-04-03 00:46:23 -0700

    this keyword

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const names = ['stephen', 'bolu', 'ada', 'bisi']
+// const names = ['stephen', 'bolu', 'ada', 'bisi']
 
 
-//MAP Returns a value and it is used to make modifications to an array
+// //MAP Returns a value and it is used to make modifications to an array
+// // const newNames = names.map(function(name){
+// //     return name.length > 3 ? name : "nope"
+// // })
+
+// //FIND
+
+// const search = names.find(function(name){
+//     return name.includes('l')
+// })
+
+
+// //FILTER
+// const shortSearch = names.filter(function(name){
+//     return name.includes('b')
+// })
+
+// //Some and Every
+// const checkLength = names.every(function(name){
+//     return name.length>2
+// })
+
+// //ARROW FUNCTIONS
 // const newNames = names.map(function(name){
 //     return name.length > 3 ? name : "nope"
 // })
 
-//FIND
+// const arrowNames = names.map(name => name.length > 3 ? name : "nope")
 
-const search = names.find(function(name){
-    return name.includes('l')
-})
+// //SORT
+// const items = ['banana', 'apple', 'orange', 'mango']
+// const ratings = [92, 56, 33, 2, 48.7, 3, 9, 10]
 
-
-//FILTER
-const shortSearch = names.filter(function(name){
-    return name.includes('b')
-})
-
-//Some and Every
-const checkLength = names.every(function(name){
-    return name.length>2
-})
-
-//ARROW FUNCTIONS
-const newNames = names.map(function(name){
-    return name.length > 3 ? name : "nope"
-})
-
-const arrowNames = names.map(name => name.length > 3 ? name : "nope")
-
-//SORT
-const items = ['banana', 'apple', 'orange', 'mango']
-const ratings = [92, 56, 33, 2, 48.7, 3, 9, 10]
-
-const desRatings = [...ratings]
-desRatings.sort((a,b) => b-a)
-console.log(desRatings)
-console.log(ratings)
+// const desRatings = [...ratings]
+// desRatings.sort((a,b) => b-a)
+// console.log(desRatings)
+// console.log(ratings)
---
+++ object.js
@@ -0,0 +1,61 @@
+// function sayHi(){
+//     console.log(`Hello there ${name}`)
+//     changeName()
+//     console.log('sayHi had ended')
+// }
+
+// function changeName(name){
+//     name = 'stephen'
+//     console.log(`we changes the name to ${name}`)
+//     console.log('changeName is finished')
+// }
+
+// let name = 'dev ed'
+// sayHi()
+
+// console.log('we are done with the code')
+
+
+//This refers to an object
+
+// const user = {
+//     name: 'stephen',
+// }
+
+// const admin = {
+//     name: 'admin'
+// }
+
+// function sayHi(){
+//     console.log(this)
+// }
+
+// user.hi = sayHi
+// admin.hi = sayHi
+
+// user.hi()
+// admin.hi()
+
+// const lists = document.querySelectorAll('li')
+
+// lists.forEach(li => {
+//     li.addEventListener('click', function(){
+//         console.log(this)
+//     })
+// })
+
+// const user = {
+//     name: 'stephen',
+//     videos: ['html', 'fcss', 'css', 'react', 'JavaScript'],
+//     greet(){
+//         console.log(`Hello there ${this.name}`)
+//         //ADD FUNCTION
+//         const getVideos = () =>{
+//             console.log(`You curentyl have ${this.videos.length} videos`)
+//         }
+//         getVideos()
+//     }
+// }
+
+// user.greet()
+
